2f586cbb482aabbe22ed48abdf0405f152e2f8e7
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-11-19 21:42:09 +0800

    done adding the components on the loginview

--- app/src/main/java/com/example/Java/Library/Management/System/App.java
+++ app/src/main/java/com/example/Java/Library/Management/System/App.java
@@ -4,11 +4,11 @@
 package com.example.Java.Library.Management.System;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        LoginView meow = new LoginView();
+        meow.setVisible(true);
+        meow.setResizable(false);
+        meow.setLocationRelativeTo(null);
     }
 }
