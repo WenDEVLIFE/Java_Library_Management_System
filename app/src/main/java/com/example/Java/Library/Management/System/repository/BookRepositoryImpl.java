7e9597a3eed5393801e7bd0ef5528fe9fd7f7f58
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-11-22 12:27:24 +0800

    Add BookRepository

---
+++ app/src/main/java/com/example/Java/Library/Management/System/repository/BookRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.example.Java.Library.Management.System.repository;
+
+abstract class BookRepository {
+    // Define abstract methods for book management here
+}
+
+public class BookRepositoryImpl extends BookRepository{
+}
